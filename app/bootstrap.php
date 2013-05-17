a94d25f4f28b265a4f84e0f5de6cf4bcb8094d97
Asher Baker <asherkin@limetech.io>
2013-05-17 04:07:42 +0000

    Added error logging.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 // libphutil provides static functions that can't be autoloaded by Composer
-require_once __DIR__.'/../vendor/facebook/libphutil/src/__phutil_library_init__.php';
+require_once __DIR__ . '/../vendor/facebook/libphutil/src/__phutil_library_init__.php';
 
 // If libphutil is the last autoloader (Composer prepends, so will never be after it), it throws on missing classes...
 spl_autoload_register(function ($class) {});
@@ -15,6 +15,25 @@ $app = new Silex\Application();
 
 $app['debug'] = false;
 $app['root'] = __DIR__ . '/..';
+
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../logs/main.log',
+    'monolog.level'   => Monolog\Logger::WARNING,
+    'monolog.name'    => 'throttle',
+));
+
+$app['monolog'] = $app->share($app->extend('monolog', function($monolog, $app) {
+    if (!$app['debug']) {
+        $monolog->pushHandler(new Monolog\Handler\NativeMailerHandler(
+            array('asherkin@limetech.org'),
+            '[Throttle] Error Report',
+            'throttle@limetech.org',
+            Monolog\Logger::CRITICAL
+        ));
+    }
+
+    return $monolog;
+}));
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
