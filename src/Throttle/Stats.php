d37e2f00492a0a8779c8da756789974205debc71
Asher Baker <asherkin@limetech.io>
2016-01-15 11:36:38 +0000

    Stats output improvements.

--- src/Throttle/Stats.php
+++ src/Throttle/Stats.php
@@ -19,9 +19,9 @@ class Stats
         $query = null;
 
         if ($function !== null) {
-            $query = $app['db']->executeQuery('SELECT DATE(timestamp) AS date, COUNT(*) AS count FROM frame JOIN crash ON id = crash WHERE timestamp > DATE_SUB(NOW(), INTERVAL 30 DAY) AND frame.thread = 0 AND frame = 0 AND module = ? AND function LIKE ? GROUP BY DATE(timestamp)', array($module, $function.'%'));
+            $query = $app['db']->executeQuery('SELECT DATE(timestamp) AS date, COUNT(*) AS count FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread WHERE timestamp > DATE_SUB(NOW(), INTERVAL 30 DAY) AND frame = 0 AND module = ? AND function LIKE ? GROUP BY DATE(timestamp)', array($module, $function.'%'));
         } else if ($module !== null) {
-            $query = $app['db']->executeQuery('SELECT DATE(timestamp) AS date, COUNT(*) AS count FROM frame JOIN crash ON id = crash WHERE timestamp > DATE_SUB(NOW(), INTERVAL 30 DAY) AND frame.thread = 0 AND frame = 0 AND module LIKE ? GROUP BY DATE(timestamp)', array($module.'%'));
+            $query = $app['db']->executeQuery('SELECT DATE(timestamp) AS date, COUNT(*) AS count FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread WHERE timestamp > DATE_SUB(NOW(), INTERVAL 30 DAY) AND frame = 0 AND module LIKE ? GROUP BY DATE(timestamp)', array($module.'%'));
         } else {
             $query = $app['db']->executeQuery('SELECT DATE(timestamp) AS date, COUNT(*) AS count FROM crash WHERE timestamp > DATE_SUB(NOW(), INTERVAL 30 DAY) GROUP BY DATE(timestamp)');
         }
@@ -48,19 +48,18 @@ class Stats
         $output = array();
 
         if ($function !== null) {
-            $data = $app['db']->executeQuery('SELECT rendered, COUNT(*) AS count FROM frame WHERE thread = 0 AND frame = 0 AND module = ? AND function LIKE ? GROUP BY rendered ORDER BY count DESC LIMIT 10', array($module, $function.'%'));
-
+            $data = $app['db']->executeQuery('SELECT rendered, COUNT(*) AS count FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread WHERE frame = 0 AND module = ? AND function LIKE ? GROUP BY rendered ORDER BY count DESC LIMIT 10', array($module, $function.'%')); 
             while ($row = $data->fetch()) {
                 $output[] = array($app->escape($row['rendered']), $row['count'], false, false);
             }
         } else if ($module !== null) {
-            $data = $app['db']->executeQuery('SELECT module, function,  COUNT(*) AS count FROM frame WHERE thread = 0 AND frame = 0 AND module LIKE ? GROUP BY function ORDER BY count DESC LIMIT 10', array($module.'%'));
+            $data = $app['db']->executeQuery('SELECT module, function,  COUNT(*) AS count FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread WHERE frame = 0 AND module LIKE ? GROUP BY function ORDER BY count DESC LIMIT 10', array($module.'%'));
 
             while ($row = $data->fetch()) {
                 $output[] = array($app->escape($row['function'] ? $row['module'].'!'.$row['function'] : $row['module']), $row['count'], $row['module'], $row['function']);
             }
         } else {
-            $data = $app['db']->executeQuery('SELECT module, function, COUNT(*) AS count FROM frame WHERE thread = 0 AND frame = 0 GROUP BY module, function ORDER BY count DESC LIMIT 10');
+            $data = $app['db']->executeQuery('SELECT module, function, COUNT(*) AS count FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread WHERE frame = 0 GROUP BY module, function ORDER BY count DESC LIMIT 10');
 
             while ($row = $data->fetch()) {
                 $output[] = array($app->escape($row['function'] ? $row['module'].'!'.$row['function'] : $row['module']), $row['count'], $row['module'], $row['function']);
@@ -76,17 +75,24 @@ class Stats
     {
         $query = null;
 
+        $limit = intval($app['request']->get('limit', 10));
+        if ($limit <= 0) {
+            $limit = 10;
+        } else if ($limit > 200) {
+            $limit = 200;
+        }
+
         if ($function !== null) {
-            $query = $app['db']->executeQuery('SELECT crash, rendered FROM frame JOIN crash ON id = crash WHERE frame.thread = 0 AND frame = 0 AND module = ? AND function LIKE ? ORDER BY timestamp DESC LIMIT 10', array($module, $function.'%'));
+            $query = $app['db']->executeQuery('SELECT crash, rendered, cmdline, avatar FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread LEFT JOIN user ON crash.owner = user.id WHERE frame = 0 AND module = ? AND function LIKE ? ORDER BY timestamp DESC LIMIT ' . $limit, array($module, $function.'%'));
         } else if ($module !== null) {
-            $query = $app['db']->executeQuery('SELECT crash, rendered FROM frame JOIN crash ON id = crash WHERE frame.thread = 0 AND frame = 0 AND module LIKE ? ORDER BY timestamp DESC LIMIT 10', array($module.'%'));
+            $query = $app['db']->executeQuery('SELECT crash, rendered, cmdline, avatar FROM frame JOIN crash ON id = crash AND crash.thread = frame.thread LEFT JOIN user ON crash.owner = user.id WHERE frame = 0 AND module LIKE ? ORDER BY timestamp DESC LIMIT ' . $limit, array($module.'%'));
         } else {
-            $query = $app['db']->executeQuery('SELECT crash, rendered FROM crash JOIN frame ON crash = id AND frame.thread = 0 AND frame = 0 ORDER BY timestamp DESC LIMIT 10');
+            $query = $app['db']->executeQuery('SELECT crash, rendered, cmdline, avatar FROM crash JOIN frame ON crash = id AND frame.thread = crash.thread AND frame = 0 LEFT JOIN user ON crash.owner = user.id ORDER BY timestamp DESC LIMIT ' . $limit);
         }
 
         $output = array();
         while ($row = $query->fetch()) {
-            $output[] = array($row['crash'], $app->escape($row['rendered']));
+            $output[] = array($row['crash'], $app->escape($row['rendered']), (empty($row['cmdline']) ? '' : md5($row['cmdline'])), $row['avatar']);
         }
 
         return $app->json($output, 200, array(
