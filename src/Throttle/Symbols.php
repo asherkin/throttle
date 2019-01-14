80d05c10a2ac9b0ba80bc4b055d7efb1f3999713
Asher Baker <asherkin@limetech.io>
2019-01-14 00:37:25 +0000

    Reject symbol files that don't have any FUNC records

--- src/Throttle/Symbols.php
+++ src/Throttle/Symbols.php
@@ -13,12 +13,37 @@ class Symbols
             $data = $app['request']->getContent();
         }
 
-        $module = head(phutil_split_lines($data, false));
+        $app['redis']->hIncrBy('throttle:stats', 'symbols:submitted', 1);
+        $app['redis']->hIncrBy('throttle:stats', 'symbols:submitted:bytes', strlen($data));
 
-        if (!preg_match('/^MODULE (?P<operatingsystem>[^ ]++) (?P<architecture>[^ ]++) (?P<id>[a-fA-F0-9]++) (?P<name>[^\\/\\\\\r\n]++)$/m', $module, $info)) {
-            $app['monolog']->addError('Invalid symbol file: ' . $module);
+        $lines = phutil_split_lines($data, false);
+
+        if (!preg_match('/^MODULE (?P<operatingsystem>[^ ]++) (?P<architecture>[^ ]++) (?P<id>[a-fA-F0-9]++) (?P<name>[^\\/\\\\\r\n]++)$/m', $lines[0], $info)) {
+            $app['monolog']->warning('Invalid symbol file: ' . $lines[0]);
+            $app['redis']->hIncrBy('throttle:stats', 'symbols:rejected:invalid', 1);
 
             return new \Symfony\Component\HttpFoundation\Response('Invalid symbol file', 400);
+        }
+
+        if ($info['operatingsystem'] === 'Linux') {
+            $functions = 0;
+
+            foreach ($lines as $line) {
+                list($type) = explode(' ', $line, 2);
+
+                if ($type === 'STACK') {
+                    break;
+                }
+
+                if ($type === 'FUNC') {
+                    $functions++;
+                }
+            }
+
+            if ($functions === 0) {
+                $app['redis']->hIncrBy('throttle:stats', 'symbols:rejected:no-functions', 1);
+                return new \Symfony\Component\HttpFoundation\Response('Symbol file had no FUNC records, please update to Accelerator 2.4.3 or later', 400);
+            }
         }
 
         $path = $app['root'] . '/symbols/public/' . $info['name'] . '/' . $info['id'];
@@ -31,6 +56,8 @@ class Symbols
         }
 
         \Filesystem::writeFile($path . '/' . $file . '.sym.gz', gzencode($data));
+
+        $app['redis']->hIncrBy('throttle:stats', 'symbols:accepted', 1);
 
         return $app['twig']->render('submit-symbols.txt.twig', array(
             'module' => $info,
