c15bdc7d6586f9d206471ec62d8e2b0be57edf39
Asher Baker <asherkin@limetech.io>
2019-05-25 17:46:19 +0000

    Bring back the footer version

--- src/Twig/AppVariable.php
+++ src/Twig/AppVariable.php
@@ -4,14 +4,22 @@ namespace App\Twig;
 
 use Symfony\Bridge\Twig\AppVariable as SymfonyAppVariable;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Process\Process;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class AppVariable
 {
+    private $kernel;
+    private $cache;
     private $inner;
     private $config;
 
-    public function __construct(SymfonyAppVariable $inner, $config)
+    public function __construct(KernelInterface $kernel, CacheInterface $cache, SymfonyAppVariable $inner, $config)
     {
+        $this->kernel = $kernel;
+        $this->cache = $cache;
         $this->inner = $inner;
         $this->config = $config;
     }
@@ -54,5 +62,24 @@ class AppVariable
     public function getConfig()
     {
         return $this->config;
+    }
+
+    public function getVersion()
+    {
+        return $this->cache->get('git_version', function (ItemInterface $item) {
+            $item->expiresAfter(60);
+
+            $process = (new Process(['/usr/bin/git', 'describe', '--abbrev=12', '--always', '--dirty=+']))
+                ->setWorkingDirectory($this->kernel->getProjectDir())
+                ->setTimeout(1);
+
+            $ret = $process->run();
+
+            if ($ret !== 0) {
+                return null;
+            }
+
+            return $process->getOutput();
+        });
     }
 }
