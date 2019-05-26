bc4af071513781bf2026f50b48279558b0a9e0b5
Asher Baker <asherkin@limetech.io>
2019-05-26 21:51:19 +0000

    Fixed up Sharing functionality

--- src/Controller/Sharing.php
+++ src/Controller/Sharing.php
@@ -2,23 +2,40 @@
 
 namespace App\Controller;
 
+use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
 class Sharing extends AbstractController
 {
+    private $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
+    /**
+     * @Route("/settings", name="settings")
+     */
+    public function settings()
+    {
+        // Placeholder until we need more settings pages.
+        return $this->redirectToRoute('share');
+    }
+
     /**
      * @Route("/settings/share", name="share")
      */
     public function share()
     {
-        if (!$app['user']) {
-            $app->abort(401);
-        }
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-        $sharing = $app['db']->executeQuery('SELECT share.user AS id, user.name, user.avatar, accepted FROM share LEFT JOIN user ON share.user = user.id WHERE share.owner = ? ORDER BY accepted IS NULL DESC, accepted DESC', array($app['user']['id']))->fetchAll();
-        $shared = $app['db']->executeQuery('SELECT share.owner AS id, user.name, user.avatar, accepted FROM share LEFT JOIN user ON share.owner = user.id WHERE share.user = ? ORDER BY accepted IS NULL DESC, accepted DESC', array($app['user']['id']))->fetchAll();
+        $user = $this->getUser();
+
+        $sharing = $this->db->executeQuery('SELECT share.user AS id, user.name, user.avatar, accepted FROM share LEFT JOIN user ON share.user = user.id WHERE share.owner = ? ORDER BY accepted IS NULL DESC, accepted DESC', array($user->getId()))->fetchAll();
+        $shared = $this->db->executeQuery('SELECT share.owner AS id, user.name, user.avatar, accepted FROM share LEFT JOIN user ON share.owner = user.id WHERE share.user = ? ORDER BY accepted IS NULL DESC, accepted DESC', array($user->getId()))->fetchAll();
 
         return $this->render('share.html.twig', [
             'sharing' => $sharing,
@@ -26,87 +43,92 @@ class Sharing extends AbstractController
         ]);
     }
 
-    public function invite()
+    /**
+     * @Route("/settings/share/invite", methods={"POST"}, name="share_invite_post")
+     */
+    public function invite_post(Request $request)
     {
-        if (!$app['user']) {
-            $app->abort(401);
-        }
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-        return $this->render('invite.html.twig');
-    }
-
-    public function invite_post()
-    {
-        if (!$app['user']) {
-            $app->abort(401);
-        }
-
-        $user = $app['request']->get('user', null);
+        $user = $request->get('user', null);
         if ($user === null) {
             $this->addFlash('error_share_invite', 'Missing Steam ID');
-            return $app->redirect($app['url_generator']->generate('share_invite'));
+            return $this->redirectToRoute('share_invite');
         }
 
         if (!ctype_digit($user) || gmp_cmp(gmp_and($user, '0xFFFFFFFF00000000'), '76561197960265728') !== 0) {
             $this->addFlash('error_share_invite', 'Invalid Steam ID');
-            return $app->redirect($app['url_generator']->generate('share_invite'));
+            return $this->redirectToRoute('share_invite');
         }
 
-        if ($user === $app['user']['id']) {
+        $currentUser = $this->getUser();
+        if ($user === $currentUser->getId()) {
             $this->addFlash('error_share_invite', 'You already have full access to your own reports');
-            return $app->redirect($app['url_generator']->generate('share_invite'));
+            return $this->redirectToRoute('share_invite');
         }
 
-        $query = $app['db']->executeQuery('SELECT accepted FROM share WHERE owner = ? AND user = ?', array($app['user']['id'], $user))->fetch();
+        $query = $this->db->executeQuery('SELECT accepted FROM share WHERE owner = ? AND user = ?', array($currentUser->getId(), $user))->fetch();
         if ($query !== false) {
             if ($query['accepted'] !== null) {
                 $this->addFlash('error_share_invite', 'You have already granted that user access');
             } else {
                 $this->addFlash('error_share_invite', 'You have already invited that user, but they have not accepted yet');
             }
-            return $app->redirect($app['url_generator']->generate('share_invite'));
+            return $this->redirectToRoute('share_invite');
         }
 
-        $app['db']->executeUpdate('INSERT IGNORE INTO user (id) VALUES (?)', array($user));
-        $app['db']->executeUpdate('INSERT INTO share (owner, user) VALUES (?, ?)', array($app['user']['id'], $user));
+        $this->db->executeUpdate('INSERT IGNORE INTO user (id) VALUES (?)', array($user));
+        $this->db->executeUpdate('INSERT INTO share (owner, user) VALUES (?, ?)', array($currentUser->getId(), $user));
 
-        $return = $app['request']->get('return', null);
-        if (!$return) {
-            $return = $app['url_generator']->generate('share');
+        $return = $request->get('return');
+        if ($return) {
+            return $this->redirect($return);
         }
 
-        return $app->redirect($return);
+        return $this->redirectToRoute('share');
     }
 
-    public function accept()
+    /**
+     * @Route("/settings/share/invite", name="share_invite")
+     */
+    public function invite()
     {
-        if (!$app['user']) {
-            $app->abort(401);
-        }
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-        $owner = $app['request']->get('owner', null);
+        return $this->render('invite.html.twig');
+    }
+
+    /**
+     * @Route("/settings/share/accept", methods={"POST"}, name="share_accept")
+     */
+    public function accept(Request $request)
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+
+        $owner = $request->get('owner', null);
         if ($owner === null || !ctype_digit($owner) || gmp_cmp(gmp_and($owner, '0xFFFFFFFF00000000'), '76561197960265728') !== 0) {
             throw new \Exception('Missing or invalid target');
         }
 
-        $app['db']->executeUpdate('UPDATE share SET accepted = NOW() WHERE owner = ? AND user = ?', array($owner, $app['user']['id']));
+        $this->db->executeUpdate('UPDATE share SET accepted = NOW() WHERE owner = ? AND user = ?', array($owner, $this->getUser()->getId()));
 
-        $return = $app['request']->get('return', null);
-        if (!$return) {
-            $return = $app['url_generator']->generate('share');
+        $return = $request->get('return');
+        if ($return) {
+            return $this->redirect($return);
         }
 
-        return $app->redirect($return);
+        return $this->redirectToRoute('share');
     }
 
-    public function revoke()
+    /**
+     * @Route("/settings/share/revoke", methods={"POST"}, name="share_revoke")
+     */
+    public function revoke(Request $request)
     {
-        if (!$app['user']) {
-            $app->abort(401);
-        }
+        $this->denyAccessUnlessGranted('ROLE_USER');
 
-        $user = $app['request']->get('user', null);
-        $owner = $app['request']->get('owner', null);
+        $user = $request->get('user', null);
+        $owner = $request->get('owner', null);
         if ($user === $owner || ($user !== null && $owner !== null)) {
             throw new \Exception('Missing or multiple targets');
         }
@@ -116,21 +138,21 @@ class Sharing extends AbstractController
                 throw new \Exception('Invalid target');
             }
 
-            $app['db']->executeUpdate('DELETE FROM share WHERE owner = ? AND user = ?', array($app['user']['id'], $user));
+            $this->db->executeUpdate('DELETE FROM share WHERE owner = ? AND user = ?', array($this->getUser()->getId(), $user));
         } else if ($owner !== null) {
             if (!ctype_digit($owner) || gmp_cmp(gmp_and($owner, '0xFFFFFFFF00000000'), '76561197960265728') !== 0) {
                 throw new \Exception('Invalid target');
             }
 
-            $app['db']->executeUpdate('DELETE FROM share WHERE owner = ? AND user = ?', array($owner, $app['user']['id']));
+            $this->db->executeUpdate('DELETE FROM share WHERE owner = ? AND user = ?', array($owner, $this->getUser()->getId()));
         }
 
-        $return = $app['request']->get('return', null);
-        if (!$return) {
-            $return = $app['url_generator']->generate('share');
+        $return = $request->get('return');
+        if ($return) {
+            return $this->redirect($return);
         }
 
-        return $app->redirect($return);
+        return $this->redirectToRoute('share');
     }
 }
 
